93b92383d3b6b7a92700b63bc9c56d257556b01f
TAMOUSS Radouane <atamousse.red@gmail.com>
2022-05-26 15:57:34 +0100

    :zap:

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -7,11 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-    }
-
-    private class GetAllTasks {
-        extends AsyncTask<>
     }
 }
